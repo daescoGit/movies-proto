0c9eb6c627f3ffdcc119ca00b1f49cbb1cbf4ceb
dan <daneskildsen1234@gmail.com>
2022-03-14 23:47:06 +0100

    v4

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -39,7 +39,8 @@ const CategoryCard = ({ cat, fetchURL, type }) => {
   };
 
   // medie slides er sat til at starte på en tilfældig slide for varriation
-  // activeIndex={RandomNr(0, data.entryCount <= 5 ? data.entryCount - 1 : 4)}
+  // jeg sætter "defaultRandomSlide" ved data load og bruger den hvis ikke state (manuelt valg) er sat
+  // filter på specifik billed størrelse for konsistens og sikker billed source
   return (
     !isLoading && data && data.entryCount !== 0 &&
     <Col>
@@ -48,39 +49,26 @@ const CategoryCard = ({ cat, fetchURL, type }) => {
         <Card.Body className="card-body-cat">
 
           <Carousel activeIndex={slideIndex !== -1 ? slideIndex : defaultRandomSlide} onSelect={handleSlideSelect} interval={null}>
-            {data.entries.slice(0, 4).map((mediaEntry, index) => {
-              const base = mediaEntry.plprogram$thumbnails
-              let thumb = '';
+            {data.entries
+              .filter((mediaEntry) => mediaEntry.plprogram$thumbnails.hasOwnProperty('orig-720x1280'))
+              .slice(0, 4)
+              .map((mediaEntry, index) => {
+                return (
+                  <Carousel.Item key={index} style={{ maxHeight: '13rem' }}>
+                    <LinkContainer style={{ cursor: 'pointer' }} to={`/${type}/${mediaEntry.tdc$urlSlug}`}>
+                      <img
+                        className="d-block w-100"
+                        src={mediaEntry.plprogram$thumbnails['orig-636x1242'].plprogram$url}
+                        alt={mediaEntry.title}
+                      />
+                    </LinkContainer>
+                    <Carousel.Caption>
+                      <h5 style={{ textShadow: '2px 2px 4px #000000' }}>{mediaEntry.title}</h5>
+                    </Carousel.Caption>
+                  </Carousel.Item>
 
-              if ('orig-636x1242' in base) {
-                thumb = base['orig-636x1242'].plprogram$url
-              } else if ('orig-720x1280' in base) {
-                thumb = base['orig-720x1280'].plprogram$url
-              } else if ('orig-669x1188' in base) {
-                thumb = base['orig-669x1188'].plprogram$url
-              } else if ('orig-470x836' in base) {
-                thumb = base['orig-470x836'].plprogram$url
-              } else if ('origin-446x792' in base) {
-                thumb = base['orig-446x792'].plprogram$url
-              }
-
-              return (
-
-                <Carousel.Item key={index} style={{ maxHeight: '13rem' }}>
-                  <LinkContainer style={{ cursor: 'pointer' }} to={`/${type}/${mediaEntry.tdc$urlSlug}`}>
-                    <img
-                      className="d-block w-100"
-                      src={thumb}
-                      alt={mediaEntry.title}
-                    />
-                  </LinkContainer>
-                  <Carousel.Caption>
-                    <h5 style={{ textShadow: '2px 2px 4px #000000' }}>{mediaEntry.title}</h5>
-                  </Carousel.Caption>
-                </Carousel.Item>
-
-              )
-            })}
+                )
+              })}
           </Carousel>
 
         </Card.Body>
